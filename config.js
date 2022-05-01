ac0cea83e99f66c0f237fba7f5da35648efa1413
OmriArama <omri.ar97@gmail.com>
2022-05-01 08:43:03 +0300

    config

---
+++ config.js
@@ -0,0 +1,10 @@
+export const config = {
+    githubApiBaseUrl: `https://api.github.com/repos`,
+    githubApiFileDownloadType:"zipball",
+    gitHubResponseType:"arraybuffer",
+    githubAuthorizationKey: "",
+    baseFolderPath:"git-scanner-files",
+    downloadedFileType:"zip",
+    projectsFilterType:"javascript"
+    
+}
